7af1eb9bcb7eefbc8f5d8e5c8d072cea9dba00ca
Vi3301 <cryptornix@gmail.com>
2023-07-05 22:17:03 +0300

    fix parser

--- main.go
+++ main.go
@@ -4,30 +4,52 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func WebScrap() {
-	resp, err := http.Get("https://ru.hexlet.io/blog/posts/participate-in-open-source?utm_source=hexlet&utm_medium=blog&utm_campaign=hexlet-blog&utm_content=open-sourse-ne-strashen&utm_term=post_261022")
+	var adresss = "https://www.study.ru/courses/elementary/osnovy"
+	urlParse, err := url.Parse(adresss)
 	if err != nil {
 		return
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Fatalf("Status code error %d %s", resp.StatusCode, resp.Status)
+	for len(adresss) > 0 {
+
+		resp, err := http.Get(urlParse.String())
+		if err != nil {
+			log.Fatalf("Failed to get the webpage: %v", err)
+			return
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			log.Fatalf("Status code error %d %s", resp.StatusCode, resp.Status)
+		}
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			log.Print(err)
+		}
+		doc.Find("div.table-wrapper").Each(func(i int, s *goquery.Selection) {
+			title := s.Text()
+			fmt.Printf("Review %d: %s\n", i, title)
+		})
+		doc.Find("a.button-fill").Each(func(i int, s *goquery.Selection) {
+			adress, ok := s.Attr("href")
+			if !ok {
+				return
+			}
+			relativeUrl, err := url.Parse(adress)
+			if err != nil {
+				fmt.Printf("failed parese URL %v", err)
+			}
+			urlParse = urlParse.ResolveReference(relativeUrl)
+			fmt.Println(urlParse)
+			adress = urlParse.String()
+		})
 	}
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
-		title := s.Find("a").Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
-	fmt.Println(doc)
 }
+
 func main() {
 	WebScrap()
-
 }
